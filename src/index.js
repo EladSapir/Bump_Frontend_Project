15fcbc7726e80e31a16a2c552fe8b9c1b9438eab
Elad Sapir <elad.1999.1999@gmail.com>
2023-03-23 15:15:05 +0200

    asdasd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log("ELADELADELADELAD");
 fetch('https://backend-project-vzn7.onrender.com/')
     .then(response => {
-        console.log("SUCCEDDED");
+        console.log("SUCCEDDED ");
     })
     .catch(error => {
         console.log("ERROR");

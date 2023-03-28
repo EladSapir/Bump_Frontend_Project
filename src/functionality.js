e691805e10baebee4f4e7313b8feae501660f999
TomerRaitsis <tomerrs81096@gmail.com>
2023-03-28 12:07:50 +0300

    req/res testing

--- src/functionality.js
+++ src/functionality.js
@@ -1,1 +1,18 @@
-console.log('aaa');
+// eslint-disable-next-line no-unused-vars
+function getResponse() {
+  fetch('https://backend-project-vzn7.onrender.com/')
+    .then((response) => response.json())
+    .then((data) => {
+      // create a new div element
+      // eslint-disable-next-line no-undef
+      const newDiv = document.createElement('div');
+
+      // add data to the div
+      newDiv.innerHTML = data;
+
+      // append the div to the body
+      // eslint-disable-next-line no-undef
+      document.body.appendChild(newDiv);
+    })
+    .catch((error) => console.error(error));
+}

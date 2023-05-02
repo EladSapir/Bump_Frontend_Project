2c311c15ba7649c2832040d25d83dd70a1eda103
TomerRaitsis <tomerrs81096@gmail.com>
2023-05-02 14:27:39 +0300

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
   {
     path: '/homePage',
     name: 'homePage',
-    component: () => import(/* webpackChunkName: "about" */ '../views/homePage.vue')
-  }
-]
+    component: () => import(/* webpackChunkName: "about" */ '../views/homePage.vue'),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;

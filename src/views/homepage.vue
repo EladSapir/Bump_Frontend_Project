154178a41482ea967659e0551eddad24cdceff03
shaiMatz <shyshir@gmail.com>
2023-05-10 16:00:17 +0300

    added header to home page

--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,19 +1,31 @@
 <template>
-
-    <div class="homepage">
-      <h1>This is an the home page</h1>
-    </div>
- 
-  </template>
+  <div class="homepage">
+    <h1>This is an the home page</h1>
+  </div>
+</template>
   
+<script>
+import header from "@/components/header.vue";
+export default {
+  name: "homepage",
+  components: {
+    header
+  },
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
   
-  <style scoped>
-  .homepage {
-    position: absolute;
+<style scoped>
+.homepage {
+  position: absolute;
   background-image: none;
   background-color: var(--pagebgcolor);
   height: 100vh;
-  width: 100vw; 
+  width: 100vw;
 }
+</style>
 
-  </style>

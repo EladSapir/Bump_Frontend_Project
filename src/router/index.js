10dd8cfc9fc928e078a0df967a03918699f7ef1a
shaiMatz <shyshir@gmail.com>
2023-05-11 23:06:15 +0300

    fixed linter error

--- src/router/index.js
+++ src/router/index.js
@@ -16,31 +16,12 @@ const routes = [
     path: '/homepage',
     name: 'homepage',
     component: () => import(/* webpackChunkName: "homepage" */ '../views/homepage.vue'),
-    meta: { requiresAuth: true } // Add requiresAuth meta field for protected route
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
-
-// Navigation guard
-router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    // Check if the user is authenticated
-    const userId = localStorage.getItem('userId');
-    if (userId) {
-      // User is authenticated, allow access
-      next();
-    } else {
-      // User is not authenticated, redirect to login page
-      next('/login_signup');
-    }
-  } else {
-    // For routes that do not require authentication, allow access
-    next();
-  }
 });
 
 export default router;
